c420a839b736225794ec3f8842f96b7855421035
Vicente Aburto <vicenteaburtog@gmail.com>
2022-07-05 18:57:42 -0400

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var nombre = 'Vicente'; // String
+var apellido = 'Aburto';
+var sueldo = 400000; // Number
+var esAlumno = true // Boolean
+var corazon = null 
+var estadoCivil = undefined
+
+console.log(apellido)
+
+
+// = asignación
+// == comparación que SOLO ve el valor
+// === compara valor y tipo
+
+//prompt
+
+// en un prompt siempre se ingresan 'string'
+
+// parseInt() = números enteros, sin decimales
+// parseFloat() = números decimales
