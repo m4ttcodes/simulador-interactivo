0ea8be38f14fd88ca1b8ff88328a7c5e2ce57183
Vicente Aburto <vicenteaburtog@gmail.com>
2022-07-05 23:45:14 -0400

    Integrando variables, condiciones y ciclos

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-var nombre = 'Vicente'; // String
-var apellido = 'Aburto';
-var sueldo = 400000; // Number
-var esAlumno = true // Boolean
-var corazon = null 
-var estadoCivil = undefined
-
-console.log(apellido)
+/**
+                   Simulador Interactivo de una compra de ingredientes necesarios para hacer un completo.
+ */
 
 
-// = asignación
-// == comparación que SOLO ve el valor
-// === compara valor y tipo
+let ingrediente = 1
 
-//prompt
+do {
+    ingrediente = prompt("Ingrese ingrediente para hacer un completo").toLowerCase()
 
-// en un prompt siempre se ingresan 'string'
+    if (ingrediente != "pan" && ingrediente != "vienesa" && ingrediente != "tomate" && ingrediente != "palta" && ingrediente != "mayonesa") {
+        alert("Ingresa un ingrediente adecuado para hacer un completo")
+    }
+    switch (ingrediente) {
+        case "pan":
+            alert("1er ingrediente seleccionado")
+            break
+        case "vienesa":
+            alert("2do ingrediente seleccionado")
+            break
+        case "tomate":
+            alert("3er ingrediente seleccionado")
+            break
+        case "palta":
+            alert("4to ingrediente seleccionado")
+            break
+        case "mayonesa":
+            alert("5to ingrediente seleccionado")
+            break
+    }
 
-// parseInt() = números enteros, sin decimales
-// parseFloat() = números decimales
+} while (ingrediente != "pan" || ingrediente != "vienesa" || ingrediente != "tomate" || ingrediente != "palta" || ingrediente != "mayonesa")
